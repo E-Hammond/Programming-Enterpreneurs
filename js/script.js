2fb553eb91b7ac8b33f89c491a51c0faece8197e
E-Hammond <emmanuel.hammond@amalitech.org>
2021-04-20 08:39:58 +0000

    Added functionality to the minimum and maximum input fields

--- js/script.js
+++ js/script.js
@@ -4,49 +4,49 @@ var users = [
         gender: 'F',
         hobby: 'pets',
         avatar: 'avatar1.png',
-        age: '5'        
+        age: 5        
     },
     {
         name: 'Betty',
         gender: 'F',
         hobby: 'pets',
         avatar: 'avatar2.png',
-        age: '25'
+        age: 25
     },
     {
         name: 'Ronald',
         gender: 'M',
         hobby: 'music',
         avatar: 'avatar3.png',
-        age: '15'
+        age: 15
     },
     {
         name: 'Christopher',
         gender: 'M',
         hobby: 'sports',
         avatar: 'avatar4.png',
-        age: '20'
+        age: 20
     },
     {
         name: 'Ximena',
         gender: 'F',
         hobby: 'reading',
         avatar: 'avatar5.png',
-        age: '10'
+        age: 10
     },
     {
         name: 'Paul',
         gender: 'M',
         hobby: 'shopping',
         avatar: 'avatar6.png',
-        age: '35'
+        age: 35
     },
     {
         name: 'Charlie',
         gender: 'M',
         hobby: 'pets',
         avatar: 'avatar7.png',
-        age: '30'
+        age: 30
     },
 ];
 
@@ -55,6 +55,14 @@ window.addEventListener('load', function() {
     var results = document.getElementById('results');
     
     function search() {
+
+        //get minAge
+        var minAgeField = document.getElementById('minAge');
+        var minAge = minAgeField.value;
+
+        //get maxAge
+        var maxAgeField = document.getElementById('maxAge');
+        var maxAge = maxAgeField.value;
         
         //get hobby
         var hobbyField = document.getElementById('hobby');
@@ -72,15 +80,20 @@ window.addEventListener('load', function() {
             //check gender
             if (gender == 'A' || gender == users[i].gender) {
                 //check hobby
-                if (hobby == '' || hobby == users[i].hobby) {
-                    resultsHtml += '<div class="person-row">\
-                           <img src="images/' + users[i].avatar + '" />\
-                           <div class="person-info">\
-                           <div>' + users[i].name + '</div>\
-                           <div>' + users[i].age + '</div>\
-                           <div>' + users[i].hobby + '</div></div>\
-                            <button>Add</button></div>'  
-                }
+                    if((hobby == '' || hobby == users[i].hobby)){
+                        if(minAge===""|| maxAge===""|| ((minAge  <= users[i].age) && (maxAge  >= users[i].age))){
+                            resultsHtml += '<div class="person-row">\
+                            <img src="images/' + users[i].avatar + '" />\
+                            <div class="person-info">\
+                            <div>' + users[i].name + '</div>\
+                            <div>' + users[i].age + '</div>\
+                            <div>' + users[i].hobby + '</div></div>\
+                             <button>Add</button></div>'  
+                        }
+           
+                    }
+ 
+                
             }
         }
         
